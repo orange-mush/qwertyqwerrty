f368bf90c9a9b2d239c426c120e6f8c8135e45c0
orange-mush <greatSumini@gmail.com>
2019-04-16 15:51:59 +0900

    add import list

--- MovieApplication.java
+++ MovieApplication.java
@@ -1,5 +1,7 @@
 ﻿import domain.Movie;
 import domain.MovieRepository;
+import domain.Reservation;
+import domain.ReservationList;
 import view.InputView;
 import view.OutputView;
 
@@ -10,7 +12,7 @@ public class MovieApplication {
         
         while(makeReservation() != 1) {
         }
-
+        
     }
 
     // 예약종료일때만 1을 반환하고 나머지 경우에 0을 반환한다.
