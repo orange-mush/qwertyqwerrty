7307769b0a879b1afc6ab51028aa668f3606be9d
orange-mush <greatSumini@gmail.com>
2019-04-16 16:21:01 +0900

    ErrorCheck sample 완성

--- domain/ErrorCheck.java
+++ domain/ErrorCheck.java
@@ -9,6 +9,8 @@ public class ErrorCheck {
         List<Movie> movies = MovieRepository.getMovies();
         for(Movie movie : movies)
             result |= movie.getMovieId == movieId;
+        if(!result)
+            System.out.println("Error! - 유효하지 않은 영화 번호입니다. 다시 입력해주세요.");
         return result;
     }
 }
