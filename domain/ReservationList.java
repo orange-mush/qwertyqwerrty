2d893e9400e9851ca88c2205e12e0e15a93eb934
orange-mush <greatSumini@gmail.com>
2019-04-16 15:49:37 +0900

    add reservation

---
+++ domain/ReservationList.java
@@ -0,0 +1,12 @@
+﻿package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservationList {
+    private static List<Reservation> reservations = new ArrayList<>();
+
+    public static void addReservation(Reservation input) {
+	reservations.add(input);
+    }
+}
